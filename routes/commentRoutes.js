f481803a3a947af72b5d60df335f234178031931
Prince Gulia <princegulia170306@gmail.com>
2026-05-24 00:22:35 +0530

    add some fixes and completed the project

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { createComment, deleteComment } = require('../controllers/commentController');
+const verifyToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.post('/:id/comments',createComment);
-router.delete('/:id/comments/:commentId',deleteComment);
+router.post('/:id/comments',verifyToken,createComment);
+router.delete('/:id/comments/:commentId',verifyToken,deleteComment);
+
+module.exports = router;
